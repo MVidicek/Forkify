3d26641ebc4e0f1fe3a13af1771bc00b7ea45d64
MVidicek <mvidicek@protonmail.com>
2022-05-29 20:13:05 +0200

    Fixed imports

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from '../../../node_modules/fractional';
+import { Fraction } from 'fractional';
 import View from './View.js';
 
 class RecipeView extends View {
